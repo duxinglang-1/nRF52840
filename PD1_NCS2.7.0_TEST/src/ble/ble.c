c4fd3a73a5bba0e90767b404e4d2e69c4b91148c
xiebiao <biao.x@augustint.com>
2024-09-29 11:12:50 +0800

    增加GPS、BLE、WIFI、IMU、AUDIO相关模块代码。

---
+++ PD1_NCS2.7.0_TEST/src/ble/ble.c
@@ -0,0 +1,21 @@
+/****************************************Copyright (c)************************************************
+** File Name:			    ble.c
+** Descriptions:			ble function main source file
+** Created By:				xie biao
+** Created Date:			2024-09-29
+** Modified Date:      		2024-09-29
+** Version:			    	V1.0
+******************************************************************************************************/
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <string.h>
+#include "logger.h"
+
+
+void BLE_init(void)
+{
+}
+
+void BleMsgProcess(void)
+{
+}
---
+++ PD1_NCS2.7.0_TEST/src/ble/ble.h
@@ -0,0 +1,14 @@
+/****************************************Copyright (c)************************************************
+** File Name:			    ble.h
+** Descriptions:			ble function main head file
+** Created By:				xie biao
+** Created Date:			2024-09-29
+** Modified Date:      		2024-09-29
+** Version:			    	V1.0
+******************************************************************************************************/
+#ifndef __BLE_H__
+#define __BLE_H__
+
+
+
+#endif/*__BLE_H__*/
